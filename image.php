e3cace2facc5b55a80e3e00176d4f366b24e0d1a
Caleb Munyasya <caleb.munyasya@devoteam.com>
2021-01-15 17:47:11 +0100

    added image for testing purposes

--- image.php
+++ image.php
@@ -4,7 +4,7 @@ $im = imagecreatetruecolor(200, 200);
 $white = imagecolorallocate($im, 255, 255, 255);
 // Line reserved to trigger the webhook in the lab. 
 $deployment = getenv("COLOR");
-
+// This is a random comment to test the webhook functionality
 if ( $deployment == 'blue') {
   $color = imagecolorallocate($im, 0, 0, 255);
 } elseif ($deployment == 'green')  {
